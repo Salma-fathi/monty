e4ca578cf34794bbfccdad68e2cb96d458595b68
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-12-15 04:33:49 +0300

    error math

--- monty.h
+++ monty.h
@@ -67,9 +67,10 @@ void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 /* math */
-void _mul(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _sub(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _add(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-
+void _add(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
 
 #endif
