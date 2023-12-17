13ff1cea285ad1fba2233deb0003cdc2731008ad
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-17 21:18:03 +0200

    pchar 4

--- fun_structions3.c
+++ fun_structions3.c
@@ -12,7 +12,6 @@ void _pchar(stack_t **stack, unsigned int line_number)
 	if ((*stack) == NULL)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-		putchar('\n');
 		exit_error(*stack);
 	}
 	for (; (*stack)->next; stack = &(*stack)->next)
