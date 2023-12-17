492a35e247a94d9516b2a93d9d3bd472f75a6b7f
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-12-18 02:51:58 +0300

    montyy

--- monty.c
+++ monty.c
@@ -1,26 +1,24 @@
 #include "monty.h"
 
 /**
-  * main - entery functions
-  *
-  * @argc: number of arguments
-  *
-  * @argv: array of arguments
-  *
-  * Return: success (0) or error code
-  */
+ * main - entry function
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: success (0) or error code
+ */
 int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
+    stack_t *stack = NULL;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
-	read_file(argv[1], &stack);
-	freeStack(stack);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return (EXIT_FAILURE);
+    }
+    read_file(argv[1], &stack);
+    freeStack(stack);
 
-	return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
-   **/
